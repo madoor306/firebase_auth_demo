fc4dfc3e33d8db38724e48b5b0e86e03273e7d76
Manjunath Adoor <manjunathadoor@CFs-MacBook-Pro.local>
2021-08-11 20:04:11 +0530

    Getting Firestore Data

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,3 +1,8 @@
+// get data
+db.collection('guides').get().then( snapshot => {
+    setupGuides(snapshot.docs);
+});
+
 // listen for auth status change
 auth.onAuthStateChanged(user => {
     if(user) {
