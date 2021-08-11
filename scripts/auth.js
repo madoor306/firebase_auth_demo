dfeedd7c999a112d5a33f2281af5497422cd3a77
Manjunath Adoor <manjunathadoor@CFs-MacBook-Pro.local>
2021-08-11 19:20:03 +0530

    Added function to check Auth change status

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,5 +1,13 @@
-// signup
+// listen for auth status change
+auth.onAuthStateChanged(user => {
+    if(user) {
+        console.log('User logged in -> ', user);
+    } else {
+        console.log('User logged out.');
+    }
+});
 
+// signup
 const signupform = document.querySelector("#signup-form");
 signupform.addEventListener('submit', (e) => {
     e.preventDefault();
@@ -10,10 +18,34 @@ signupform.addEventListener('submit', (e) => {
     
     // sign up the user
     auth.createUserWithEmailAndPassword(email, password).then(cred => {
-        console.log(cred.user);
         const modal = document.querySelector('#modal-signup');
         M.Modal.getInstance(modal).close();
         signupform.reset();
-    })
+    });
 
-})
+});
+
+// logout 
+const logout = document.querySelector('#logout');
+logout.addEventListener('click', e => {
+    e.preventDefault();
+    auth.signOut();
+});
+
+//login
+const loginForm = document.querySelector('#login-form');
+loginForm.addEventListener('submit', e => {
+    e.preventDefault();
+
+    // Get User info
+    const email = loginForm['login-email'].value;
+    const password = loginForm['login-password'].value;
+
+    //login the user
+    auth.signInWithEmailAndPassword(email, password).then(cred => {
+        // close the login modal and reset the form
+        const modal = document.querySelector('#modal-login');
+        M.Modal.getInstance(modal).close();
+        loginForm.reset();
+    });
+});
